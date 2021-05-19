4f08ada0f847eab6ff993b617d9ac84bfaafec18
mohit2023 <mohit7153@gmail.com>
2021-05-19 23:10:20 +0530

    name change

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]){
   
   game = new Game();
 
-  game->init("Pacman", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+  game->init("2-Player-Maze-Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
   while(game->running()){
     frameStart = SDL_GetTicks();
